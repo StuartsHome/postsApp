872256cd8fd68d1fd7608ae8cc95402ec0c4b9d3
Stuart Smith <stuartsayswhat@googlemail.com>
2021-04-20 16:41:48 +0100

    added mysql support

--- db.go
+++ db.go
@@ -1,19 +1,47 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
+type User struct {
+	Name string `json:"name"`
+}
+
 func main() {
 	fmt.Println("Go MYSQL Tutorial")
 
-	db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)first_instance")
+	db, err := sql.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/go_db")
 	if err != nil {
 		panic(err.Error())
 	}
 
 	defer db.Close()
 
-	fmt.Println("Successfully connected to MySQL database")
-}wo
+	results, err := db.Query("SELECT name FROM users")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for results.Next() {
+		var user User
+		err = results.Scan(&user.Name)
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Println(user.Name)
+	}
+
+	// insert, err := db.Query("INSERT INTO users VALUES(1, 'Stuart', 20)")
+
+	// if err != nil {
+	// 	panic(err.Error())
+	// }
+
+	// defer insert.Close()
+	// fmt.Println("Successfully inserted into user table")
+
+}
